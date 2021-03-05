d8ec283f9ea6024fc265bc8eff5adbe39aef4584
Prathamesh <dev.prathameshpadval@gmail.com>
2021-03-05 22:29:10 +0530

    Initial Release

---
+++ src/main/java/com/dev/wellness/HannockWellnessApplication.java
@@ -0,0 +1,13 @@
+package com.dev.wellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannockWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannockWellnessApplication.class, args);
+	}
+
+}
